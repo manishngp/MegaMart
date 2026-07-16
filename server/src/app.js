746b9eaf334eccbc2ffc7e070a0c9a42020263ed
manishngp <manishngpdoc@gmail.com>
2026-07-16 23:38:12 +0530

    Initial project structure

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "E-Commerce API is running successfully ",
+  });
+});
+
+export default app;
